a4db33e62c7f68642b3ea606f1891fa2eca93c2e
AntoineDrsl <antoine.durussel@gmail.com>
2020-05-26 12:00:08 +0200

    FEAT leaflet map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MapComponent
   ],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
