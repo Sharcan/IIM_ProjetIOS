74fb9cb5af459f1a2c9737716446c304128a2d22
AntoineDrsl <antoine.durussel@gmail.com>
2020-05-27 14:52:17 +0200

    FIX a lot of things

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,21 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { AddressService } from './../../shared/address.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss'],
+  styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
 
-  constructor(private modalController: ModalController) { }
+  addresses;
 
-  ngOnInit() {}
+  constructor(private modalController: ModalController, public addressService: AddressService) { }
+
+  ngOnInit() {
+    this.addressService.load().then(data => {
+      this.addresses = data;
+    })
+  }
 
   dismiss() {
     this.modalController.dismiss({
       'dismissed': true
     })
+  }
+
+  loadAddress() {
+    this.addressService.load().then(data => {
+      this.addresses = data;
+      console.log(this.addresses);
+    });
   }
 
 }
